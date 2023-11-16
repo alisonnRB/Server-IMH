3e78cb2c5829038e65c0f27261c20147d33495ae
KrisCeu <yhls2004@gmail.com>
2023-11-15 21:17:54 -0300

    validações

--- server/newSenha.php
+++ server/newSenha.php
@@ -2,6 +2,7 @@
 include "./conexão/conexao.php";
 include "./resposta/resposta.php";
 include "./token/decode_token.php";
+include "./validações/validacoes.php";
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
@@ -20,20 +21,39 @@ if($token == "erro"){
 function change_senha($id, $body) {
     //! validar se as entradas existem e etc.
 
-    $conexao = conecta_bd();
-    //! validar conexao
+    $senhaOG = validar_senha($body->senhaAntiga);
+    if ($senhaOG[0] == true){
+        $senhaOG = $senhaOG[1];
+    } else {
+        resposta (401, false, $senhaOG[1]);
+    }
 
+    $senhaNew = validar_senha($body->NovaSenha);
+    if ($senhaNew[0] == true){
+        $senhaNew = $senhaNew[1];
+    } else {
+        resposta (401, false, $senhaNew[1]);
+    }
+
+    $conexao = conecta_bd();
+    if (!$conexao) {
+        resposta(500, false, "Houve um problema ao conectar ao servidor");
+    } else {
     $consulta = $conexao->prepare('SELECT senha FROM usuarios WHERE id = :id');
     $consulta->bindParam(':id', $id);
     $consulta->execute();
     $Senha = $consulta->fetchColumn();
+    }
 
     //! verificar com hash
 
-    if($Senha == $body->senhaAntiga){
+
+        if(password_verify($senhaOG, $Senha)){
         //!converter a senha para hash
+        $cripto_senha = cripto_senha($senhaNew);
         $stmt = $conexao->prepare('UPDATE usuarios SET senha = ? WHERE id = ?');
-        $stmt->execute([$body->NovaSenha, $id]);
+        $stmt->execute([$cripto_senha, $id]);
+
 
         resposta(200, true, 'senha alterada');
     }else{
@@ -41,3 +61,5 @@ function change_senha($id, $body) {
     }
 
 }
+
+?>
