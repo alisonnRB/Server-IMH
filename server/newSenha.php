7407b236532cc8fee14156e8144e36f6f310331f
alisonnRB <alisonrbjs@gmail.com>
2023-11-26 17:02:36 -0300

    senha att

--- server/newSenha.php
+++ server/newSenha.php
@@ -15,67 +15,73 @@ $body = json_decode($body);
 
 
 $token = decode_token($body->id);
-if(!$token || $token == "erro"){
+if (!$token || $token == "erro") {
     resposta(200, false, "não autorizado");
-}else{
+} else {
     change_senha($token->id, $body);
 }
 
-function change_senha($id, $body) {
+function change_senha($id, $body)
+{
     //! validar se as entradas existem e etc.
+    if ($body->tipo != 'ihm') {
+        if ($body->idioma == "PT") {
+            resposta(200, false, 'Sua conta é do Google!');
+        } else if ($body->idioma == "ES") {
+            resposta(200, false, '¡Tu cuenta es Google!');
+        } else if ($body->idioma == "EN") {
+            resposta(200, false, 'Your account is Google!');
+        }
+    }
 
     $senhaOG = validar_senha($body->senhaAntiga);
-    if ($senhaOG[0] == true){
+    if ($senhaOG[0] == true) {
         $senhaOG = $senhaOG[1];
     } else {
-        resposta (200, false, $senhaOG[1]);
+        resposta(200, false, $senhaOG[1]);
     }
 
     $senhaNew = validar_senha($body->NovaSenha);
-    if ($senhaNew[0] == true){
+    if ($senhaNew[0] == true) {
         $senhaNew = $senhaNew[1];
     } else {
-        resposta (200, false, $senhaNew[1]);
+        resposta(200, false, $senhaNew[1]);
     }
 
     $conexao = conecta_bd();
     if (!$conexao) {
         resposta(200, false, "Houve um problema ao conectar ao servidor");
     } else {
-    $consulta = $conexao->prepare('SELECT senha FROM usuarios WHERE id = :id');
-    $consulta->bindParam(':id', $id);
-    $consulta->execute();
-    $Senha = $consulta->fetchColumn();
-    
+        $consulta = $conexao->prepare('SELECT senha FROM usuarios WHERE id = :id');
+        $consulta->bindParam(':id', $id);
+        $consulta->execute();
+        $Senha = $consulta->fetchColumn();
 
-    //! verificar com hash
-        if(password_verify($senhaOG, $Senha)){
-        //!converter a senha para hash
-        $cripto_senha = cripto_senha($senhaNew);
-        $stmt = $conexao->prepare('UPDATE usuarios SET senha = ? WHERE id = ?');
-        $stmt->execute([$cripto_senha, $id]);
 
-        if ($body->idioma == "PT"){
-            resposta(200, true, 'senha alterada');
-        }
-        else if ($body->idioma == "ES"){
-            resposta(200, true, 'contraseña cambiada');
-        }
-        else if ($body->idioma == "EN"){
-            resposta(200, true, 'password changed');
-        }
-        }else{
-            if ($body->idioma == "PT"){
+        //! verificar com hash
+        if (password_verify($senhaOG, $Senha)) {
+            //!converter a senha para hash
+            $cripto_senha = cripto_senha($senhaNew);
+            $stmt = $conexao->prepare('UPDATE usuarios SET senha = ? WHERE id = ?');
+            $stmt->execute([$cripto_senha, $id]);
+
+            if ($body->idioma == "PT") {
+                resposta(200, true, 'senha alterada');
+            } else if ($body->idioma == "ES") {
+                resposta(200, true, 'contraseña cambiada');
+            } else if ($body->idioma == "EN") {
+                resposta(200, true, 'password changed');
+            }
+        } else {
+            if ($body->idioma == "PT") {
                 resposta(200, false, 'essa não é sua senha');
-            }
-            else if ($body->idioma == "ES"){
+            } else if ($body->idioma == "ES") {
                 resposta(200, false, 'esta no es tu contraseña');
-            }
-            else if ($body->idioma == "EN"){
+            } else if ($body->idioma == "EN") {
                 resposta(200, false, 'this is not your password');
             }
         }
-    }   
+    }
 }
 
 ?>
