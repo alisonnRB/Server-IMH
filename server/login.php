09d5db98afb3ffdf8aad1be0595bbb5678128908
alisonnRB <alisonrbjs@gmail.com>
2023-11-29 17:22:49 -0300

    ajuste

--- server/login.php
+++ server/login.php
@@ -13,7 +13,6 @@ header('Access-Control-Allow-Headers: *');
 
 function verifica($body){
     //! Verificar entrada string, filtrar e etc
-    sleep(5);
     //? verifica se estão vazios
     if (empty($body->email) && empty($body->senha)) {
         resposta(200, false, "Você deve preencher os campos");
