5946db2999f1cbd512de25b383bae4a3be5e70e0
alisonnRB <alisonrbjs@gmail.com>
2023-11-29 13:06:15 -0300

    ordenado contatos

--- server/busca_amigos.php
+++ server/busca_amigos.php
@@ -29,9 +29,8 @@ function busca_amigos($id_user){
         $consulta->execute();
         $seguidores = $consulta->fetchAll(PDO::FETCH_ASSOC);
 
-        $list = new stdClass();
+        $list = array();
 
-        $i = 0;
         foreach ($seguidores as $seguidor) {
             if($seguidor['user_id'] == $id_user){
                 $stm = $conexao->prepare("SELECT id, user_id, id_ref FROM seguidores WHERE id_ref = :id_ref AND user_id = :user_id");
@@ -40,16 +39,42 @@ function busca_amigos($id_user){
                 $stm->execute();
                 $amigos = $stm->fetchAll(PDO::FETCH_ASSOC); 
                 if ($amigos){
-                    $list->{$i} = $amigos;
-                    $i++;
+                    $list[] = $amigos[0];
                 }
             }
         } 
+
+        foreach ($list as &$amigo) {
+            // Contar mensagens não lidas
+            $msgsNaoLidas = $conexao->prepare("SELECT COUNT(*) AS nao_lidas FROM chats WHERE (id_user1 = :id OR id_user2 = :id) AND (id_user1 = :id2 OR id_user2 = :id2) AND visu = 0");
+            $msgsNaoLidas->bindParam(":id", $amigo['user_id']);
+            $msgsNaoLidas->bindParam(":id2", $amigo['id_ref']);
+            $msgsNaoLidas->execute();
+            $resultNaoLidas = $msgsNaoLidas->fetch(PDO::FETCH_ASSOC);
+
+            $amigo['msgs_nao_lidas'] = $resultNaoLidas['nao_lidas'];
+
+            // Contar total de mensagens
+            $msgsTotal = $conexao->prepare("SELECT COUNT(*) AS total_msgs FROM chats WHERE (id_user1 = :id OR id_user2 = :id) AND (id_user1 = :id2 OR id_user2 = :id2)");
+            $msgsTotal->bindParam(":id", $amigo['user_id']);
+            $msgsTotal->bindParam(":id2", $amigo['id_ref']);
+            $msgsTotal->execute();
+            $resultTotal = $msgsTotal->fetch(PDO::FETCH_ASSOC);
+
+            $amigo['total_msgs'] = $resultTotal['total_msgs'];
+        }
+
+        // Ordenar o array com base em mensagens não lidas e, em seguida, no total de mensagens
+        usort($list, function ($a, $b) {
+            if ($a['msgs_nao_lidas'] != $b['msgs_nao_lidas']) {
+                return $b['msgs_nao_lidas'] - $a['msgs_nao_lidas'];
+            } else {
+                return $b['total_msgs'] - $a['total_msgs'];
+            }
+        });
+
         resposta(200, true, $list);  
     }
     resposta(200, true, false); 
 }
-
-
-
 ?>
