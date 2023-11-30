88f85fd01f5582ed8cf78f2dcbae6c71cfa23162
alisonnRB <alisonrbjs@gmail.com>
2023-11-30 20:39:44 -0300

    ajsute

--- server/busca_curtidas.php
+++ server/busca_curtidas.php
@@ -12,20 +12,6 @@ header('Access-Control-Allow-Methods: POST');
 $body = file_get_contents('php://input');
 $body = json_decode($body);
 
-$id_ref = validar_int($body->id_ref);
-if ($id_ref[0] == true) {
-    $id_ref = $id_ref[1];
-} else {
-    resposta(200, false, $id_ref[1]);
-}
-
-//validar body->tipo 
-$tipo = validar_string($body->tipo);
-if ($tipo[0] == true) {
-    $tipo = $tipo[1];
-} else {
-    resposta(200, false, $tipo[1]);
-}
 
 $token = decode_token($body->id);
 if (!$token || $token == "erro") {
@@ -43,7 +29,7 @@ function busca_curtidas($id, $tipo)
     } else {
 
         $consulta = $conexao->prepare("SELECT id, id_user, id_ref, tipo, coment FROM curtidas WHERE id_ref = :id_ref AND tipo = :tipo");
-        $consulta->bindParam(':id_ref', $id_ref);
+        $consulta->bindParam(':id_ref', $id);
         $consulta->bindParam(':tipo', $tipo);
         $consulta->execute();
         $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
