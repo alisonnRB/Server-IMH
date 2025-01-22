dea6b337baa3899762e2ae0c663a105da8cfb197
alisonnRB <alisonrbjs@gmail.com>
2025-01-22 19:34:23 -0300

    att

--- server/conexão/conexao.php
+++ server/conexão/conexao.php
@@ -1,12 +1,18 @@
 <?php
 function conecta_bd()
 {
+    echo (getenv('DB_HOST'));
+    echo getenv('DB_PORT');
+    echo getenv('DB_NAME');
+    echo getenv('DB_USER');
+    echo getenv('DB_PASSWORD');
+
     // Obtendo os dados de conexão das variáveis de ambiente
-    $host = getenv('DB_HOST') ?: 'localhost';
+    $host = getenv('DB_HOST') ?: 'dpg-cu8kbdaj1k6c73a002og-a';
     $port = getenv('DB_PORT') ?: '5432';
-    $dbname = getenv('DB_NAME') ?: 'ihm';
-    $user = getenv('DB_USER') ?: 'root';
-    $password = getenv('DB_PASSWORD') ?: '';
+    $dbname = getenv('DB_NAME') ?: 'ihm_database';
+    $user = getenv('DB_USER') ?: 'ihm_database_user';
+    $password = getenv('DB_PASSWORD') ?: '6VH0P3ugUBq22ReQZMGWv1Fxk8pMufEj';
 
     try {
         // DSN para PostgreSQL
