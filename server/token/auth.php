29fb51dc31a1f70cf9e87b8284a4cfcf56de71c8
alisonnRB <alisonrbjs@gmail.com>
2025-01-23 19:06:09 -0300

    token adjust

--- server/token/auth.php
+++ server/token/auth.php
@@ -1,36 +1,40 @@
 <?php
-    include "../resposta/resposta.php";
-    include "../../vendor/autoload.php";
-    
-    use \Firebase\JWT\JWT;
-    use \Firebase\JWT\Key;
+include "../resposta/resposta.php";
+include "../../vendor/autoload.php";
 
-    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__, 3));
-    $dotenv->load();
+echo "oi trouxa";
 
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS'); 
-    header('Access-Control-Allow-Headers: Content-Type, Authorization, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With'); 
+use \Firebase\JWT\JWT;
+use \Firebase\JWT\Key;
 
-    $auth = $_SERVER['HTTP_AUTHORIZATION'];
-    
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__, 3));
+$dotenv->load();
 
-    $token = str_replace('Bearer ','', $auth);
-    
-    try{
-        $decoded = JWT::decode($token, new Key($_SERVER['KEY'], 'HS256'));
-        resposta(200, true, "autenticado"); 
-    }catch (ExpiredException $e) {
-        resposta(200, false, 'Token expirado');
-    }catch (BeforeValidException $e) {
-        resposta(200, false, 'Token ainda não é válido');
-    }catch (SignatureInvalidException $e) {
-        resposta(200, false, 'Token inválido - Assinatura inválida');
-    }catch(Throwable $e){
-        if($e->getMessage() == 'Expired token'){
-            resposta(200, false, 'token Expirado');     
-        }
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
+
+$auth = $_SERVER['HTTP_AUTHORIZATION'];
+
+
+$token = str_replace('Bearer ', '', $auth);
+
+echo $token;
+
+try {
+    $decoded = JWT::decode($token, new Key($_SERVER['KEY'], 'HS256'));
+    resposta(200, true, "autenticado");
+} catch (ExpiredException $e) {
+    resposta(200, false, 'Token expirado');
+} catch (BeforeValidException $e) {
+    resposta(200, false, 'Token ainda não é válido');
+} catch (SignatureInvalidException $e) {
+    resposta(200, false, 'Token inválido - Assinatura inválida');
+} catch (Throwable $e) {
+    if ($e->getMessage() == 'Expired token') {
+        resposta(200, false, 'token Expirado');
     }
+}
 
-    
+
 ?>
