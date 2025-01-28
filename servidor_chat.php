1adb601bd58ce9599a3c68aa7f2c2dc53207d546
alisonnRB <alisonrbjs@gmail.com>
2025-01-28 01:30:41 -0300

    novas configurações de protocolo

--- servidor_chat.php
+++ servidor_chat.php
@@ -4,7 +4,7 @@ use Api\WebSocket\SistemaChat;
 use Ratchet\Http\HttpServer;
 use Ratchet\Server\IoServer;
 use Ratchet\WebSocket\WsServer;
-use Ratchet\WebSocket\SecureWebSocketServer; // Importar a classe SecureWebSocketServer
+
 
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/server/conexão/conexao.php';
@@ -13,7 +13,6 @@ $conexaoBD = conecta_bd();
 
 // Certifique-se de que $conexaoBD seja uma instância do PDO
 if ($conexaoBD instanceof PDO) {
-    // Criação do servidor WebSocket com suporte a SSL/TLS
     $server = IoServer::factory(
         new HttpServer(
             new WsServer(
@@ -21,17 +20,10 @@ if ($conexaoBD instanceof PDO) {
             )
         ),
         8080,
-        '0.0.0.0'  // Porta 8080, modifique conforme necessário
+        '0.0.0.0'
     );
 
-    // Configuração de SSL/TLS com os arquivos de certificado
-    $secureServer = new SecureWebSocketServer(
-        $server,
-        '/path/to/your/cert.pem',   // Caminho para o seu certificado SSL
-        '/path/to/your/key.pem'     // Caminho para a chave privada do certificado
-    );
-
-    $secureServer->run();  // Executa o servidor WebSocket seguro
+    $server->run();
 } else {
     echo "A conexão PDO não está correta.";
 }
